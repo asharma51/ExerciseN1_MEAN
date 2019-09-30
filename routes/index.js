db987eeccd2242f81286d3ecb4de9b8b7195db4a
Apoorva Sharma <asharma51@horizon.csueastbay.edu>
2019-09-30 04:01:23 -0700

    connected mongodb

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 //connect with mongodb and fetch records from Users
 var mongodb = require('mongodb');
 var DATABASE_URL = 'mongodb://asharma51:a1s2d3f4@ds215380.mlab.com:15380/heroku_vd9bt3bv';
-app.get('/db', function (request, response) {
+router.get('/db', function (request, response) {
   mongodb.MongoClient.connect(DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM Users', function(err, result) {
       done();
