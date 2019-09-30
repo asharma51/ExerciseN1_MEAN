d61ba9c7473ba57d482243a79f04b75b5ab9c5e4
Apoorva Sharma <asharma51@horizon.csueastbay.edu>
2019-09-30 04:36:00 -0700

    connected mongodb

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,19 @@ router.get('/', function(req, res, next) {
 var mongodb = require('mongodb');
 var DATABASE_URL = 'mongodb://asharma51:a1s2d3f4@ds215380.mlab.com:15380/heroku_vd9bt3bv';
 router.get('/db', function (request, response) {
-  mongodb.MongoClient.connect(DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM Users', function(err, result) {
-      done();
-      if (err)
-      { console.error(err); response.send("Error " + err); }
-      else
-      { response.render('db', {results: result.rows} ); }
+  mongodb.MongoClient.connect(DATABASE_URL, function(err, db) {
+    if(err) throw err;
+    var Users = db.collection('Users');
+    Users.find({}).toArray(function(err,result){
+      if(err) throw err;
+      response.render('db', {results: result});
     });
+
+    //close connection when your app is terminating.
+    db.close(function (err) {
+      if(err) throw err;
+    });
+
   });
 });
 module.exports = router;
