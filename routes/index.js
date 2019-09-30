e9b3120c4fddcd318d353b5a6f5ac16dc053f242
Apoorva Sharma <asharma51@horizon.csueastbay.edu>
2019-09-30 03:48:16 -0700

    connected mongodb

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//connect with mongodb and fetch records from Users
+var mongodb = require('mongodb');
+var DATABASE_URL = 'mongodb://asharma51:a1s2d3f4@ds215380.mlab.com:15380/heroku_vd9bt3bv';
+app.get('/db', function (request, response) {
+  mongodb.MongoClient.connect(DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM Users', function(err, result) {
+      done();
+      if (err)
+      { console.error(err); response.send("Error " + err); }
+      else
+      { response.render('db', {results: result.rows} ); }
+    });
+  });
+});
 module.exports = router;
